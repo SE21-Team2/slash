0d367c31b3a2190833ad27e4d29150fd8d2a5710
pchetan <pchetan@ncsu.edu>
2021-11-29 22:18:00 -0500

    update tests

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -50,75 +50,84 @@ def test_user(client) -> None:
 
 
 def test_wishlist(client) -> None:
-    # create 2 test users
-    rv = client.post('/signup/', json={"username": "test_user_1", "password": "password"})
-    data = json.loads(rv.get_data(as_text=True))
-    assert data['valid']  # is True
-    rv = client.post('/signup/', json={"username": "test_user_2", "password": "password"})
-    data = json.loads(rv.get_data(as_text=True))
-    assert data['valid']  # is True
+    try:
+        # create 2 test users
+        rv = client.post('/signup/', json={"username": "test_user_1", "password": "password"})
+        data = json.loads(rv.get_data(as_text=True))
+        assert data['valid']  # is True
+        rv = client.post('/signup/', json={"username": "test_user_2", "password": "password"})
+        data = json.loads(rv.get_data(as_text=True))
+        assert data['valid']  # is True
 
-    # try:
-    # nothing in wishlist for both
-    rv = client.get('/wishlist/?user=test_user_1')
-    assert len(rv.json) == 0
-    rv = client.get('/wishlist/?user=test_user_2')
-    assert len(rv.json) == 0
+        # try:
+        # nothing in wishlist for both
+        rv = client.get('/wishlist/?user=test_user_1')
+        assert len(rv.json) == 0
+        rv = client.get('/wishlist/?user=test_user_2')
+        assert len(rv.json) == 0
 
-    # add one for each
-    item = dict(title="socks", price="15.99", website="amazon", link="www.amazon.com/socks", rating=5)
-    rv = client.post('/wishlistAdd/', json={"user": "test_user_1", "item": item})
-    assert rv.status_code == 200  # OK
-    item = dict(title="shoes", price="55.99", website="amazon", link="www.amazon.com/shoes", rating=5)
-    rv = client.post('/wishlistAdd/', json={"user": "test_user_2", "item": item})
-    assert rv.status_code == 200  # no response
-    item = dict(title="shirt", price="25.99", website="amazon", link="www.amazon.com/shirt", rating=5)
-    rv = client.post('/wishlistAdd/', json={"user": "test_user_2", "item": item})
-    assert rv.status_code == 200  # no response
+        # add one for each
+        item = dict(title="socks", price="15.99", website="amazon", link="www.amazon.com/socks", rating=5, img_link="www.amazon.com/socks_image")
+        rv = client.post('/wishlistAdd/', json={"user": "test_user_1", "item": item})
+        assert rv.status_code == 200  # OK
+        item = dict(title="shoes", price="55.99", website="amazon", link="www.amazon.com/shoes", rating=5, img_link="www.amazon.com/shoes_image")
+        rv = client.post('/wishlistAdd/', json={"user": "test_user_2", "item": item})
+        assert rv.status_code == 200  # no response
+        item = dict(title="shirt", price="25.99", website="amazon", link="www.amazon.com/shirt", rating=5, img_link="www.amazon.com/shirt_image")
+        rv = client.post('/wishlistAdd/', json={"user": "test_user_2", "item": item})
+        assert rv.status_code == 200  # no response
 
-    # get it and verify added items are there
-    rv = client.get('/wishlist/?user=test_user_1')
-    result = rv.json
-    assert len(result) == 1
+        # get it and verify added items are there
+        rv = client.get('/wishlist/?user=test_user_1')
+        result = rv.json
+        assert len(result) == 1
 
-    expected = {"title": "socks", "price": "15.99", "website": "amazon", "link": "www.amazon.com/socks", "rating": "5"}
-    for key in expected:
-        if key in result and expected[key] == result[key]:
-            assert True
-    rv = client.get('/wishlist/?user=test_user_2')
-    result = rv.json
-    assert len(result) == 2
-    expected = [
-        {"title": "shoes", "price": "55.99", "website": "amazon", "link": "www.amazon.com/shoes", "rating": "5"},
-        {"title": "shirt", "price": "25.99", "website": "amazon", "link": "www.amazon.com/shirt", "rating": "5"}
-    ]
-    for key in expected[0]:
-        if key in result[0] and expected[0][key] == result[0][key]:
-            assert True
-    for key in expected[1]:
-        if key in result[1] and expected[1][key] == result[1][key]:
-            assert True
+        expected = {"title": "socks", "price": "15.99", "website": "amazon", "link": "www.amazon.com/socks", "rating": "5", "img_link": "www.amazon.com/socks_image"}
+        for key in expected:
+            if key in result and expected[key] == result[key]:
+                assert True
+        rv = client.get('/wishlist/?user=test_user_2')
+        result = rv.json
+        assert len(result) == 2
+        expected = [
+            {"title": "shoes", "price": "55.99", "website": "amazon", "link": "www.amazon.com/shoes", "rating": "5", "img_link": "www.amazon.com/shoes_image"},
+            {"title": "shirt", "price": "25.99", "website": "amazon", "link": "www.amazon.com/shirt", "rating": "5", "img_link": "www.amazon.com/shirt_image"}
+        ]
+        for key in expected[0]:
+            if key in result[0] and expected[0][key] == result[0][key]:
+                assert True
+        for key in expected[1]:
+            if key in result[1] and expected[1][key] == result[1][key]:
+                assert True
 
-    item = dict(title="socks", price="15.99", website="amazon", link="www.amazon.com/socks", rating="5")
-    rv = client.delete('/wishlistRemove/',
-                       json={"user": "test_user_1", "item": item})
-    assert rv.status_code == 200  # OK
-    rv = client.delete('/wishlistClear/', json={"user": "test_user_2"})
-    assert rv.status_code == 200  # OK
+        item = dict(title="socks", price="15.99", website="amazon", link="www.amazon.com/socks", rating="5", img_link="www.amazon.com/socks_image")
+        rv = client.delete('/wishlistRemove/',
+                        json={"user": "test_user_1", "item": item})
+        assert rv.status_code == 200  # OK
+        rv = client.delete('/wishlistClear/', json={"user": "test_user_2"})
+        assert rv.status_code == 200  # OK
 
-    # nothing in wishlist for both
-    rv = client.get('/wishlist/?user=test_user_1')
-    print(rv)
-    assert len(rv.json) == 0
-    assert rv.status_code == 200  # OK
+        # nothing in wishlist for both
+        rv = client.get('/wishlist/?user=test_user_1')
+        print(rv)
+        assert len(rv.json) == 0
+        assert rv.status_code == 200  # OK
 
-    rv = client.get('/wishlist/?user=test_user_2')
-    assert len(rv.json) == 0
+        rv = client.get('/wishlist/?user=test_user_2')
+        assert len(rv.json) == 0
 
-    # cleanup test users
-    rv = client.delete('/deleteuser/', json={"username": "test_user_1", "password": "password"})
-    data = json.loads(rv.get_data(as_text=True))
-    assert data['valid']  # is True
-    rv = client.delete('/deleteuser/', json={"username": "test_user_2", "password": "password"})
-    data = json.loads(rv.get_data(as_text=True))
-    assert data['valid']  # is True
+        # cleanup test users
+        rv = client.delete('/deleteuser/', json={"username": "test_user_1", "password": "password"})
+        data = json.loads(rv.get_data(as_text=True))
+        assert data['valid']  # is True
+        rv = client.delete('/deleteuser/', json={"username": "test_user_2", "password": "password"})
+        data = json.loads(rv.get_data(as_text=True))
+        assert data['valid']  # is True
+
+    except:
+        client.delete('/wishlistClear/', json={"user": "test_user_1"})
+        client.delete('/wishlistClear/', json={"user": "test_user_2"})
+        client.delete('/deleteuser/', json={"username": "test_user_1", "password": "password"})
+        client.delete('/deleteuser/', json={"username": "test_user_2", "password": "password"})
+        
+        assert False
